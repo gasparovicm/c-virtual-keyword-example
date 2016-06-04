cbc548073ba6f7ba61471a9987c5435e3b93008a
Martin Gasparovic <gasparovicm@gmail.com>
2016-06-04 14:08:45 +0200

    Initial "release" of c# virtual, everride, new and hidden example.

---
+++ VirtualAndNewExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace VirtualAndNewExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseClass = new BaseClass();
+            var derivedVirtual = new DerivedVirtual();
+            var derivedNew = new DerivedNew();
+            var derivedHidden = new DerivedHidden();
+
+            Trace.WriteLine("Not casted versions");
+            baseClass.Name();
+            derivedVirtual.Name();
+            derivedNew.Name();
+            derivedHidden.Name();
+            Trace.WriteLine("Casted to base versions");
+            baseClass.Name();
+            ((BaseClass)derivedVirtual).Name();
+            ((BaseClass)derivedNew).Name();
+            ((BaseClass)derivedHidden).Name();
+        }
+    }
+
+    public class BaseClass
+    {
+        public virtual void Name()
+        {
+            Trace.WriteLine("Base");
+        }
+    }
+
+    public class DerivedVirtual : BaseClass
+        {
+            public override void Name()
+            {
+                Trace.WriteLine("DerivedVirtual");
+            }
+        }
+
+    public class DerivedNew : BaseClass
+    {
+        public new void Name()
+        {
+            Trace.WriteLine("DerivedNew");
+        }
+    }
+
+    public class DerivedHidden : BaseClass
+    {
+        public void Name()
+        {
+            Trace.WriteLine("DerivedHidden");
+        }
+    }
+
+}
